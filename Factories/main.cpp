71929860fd7e75fdfd352b6e30f7b8933b962376
matthew.dowling <mjd117@gmail.com>
2017-02-07 14:19:45 -0800

    changing and moving particles

--- Factories/main.cpp
+++ Factories/main.cpp
@@ -1,15 +1,42 @@
 #include "sfwdraw.h"
-
+#include "math.h"
+#include <iostream>
+#include "particle.h"
 void main()
 {
 	sfw::initContext();
+
+
 	unsigned sprite =
 		sfw::loadTextureMap("../res/particle.png");
 
+	particle part;
+	part.sprite = sprite;
+	part.pos = vec2{ 400,300 };
+	part.vel = randDir(0, 360) * lerp(20, 80, rand01());
+	part.sDim = randRange(vec2{ 8,8 }, vec2{ 64,64 });
+	part.eDim = randRange(vec2{ 256,256 }, vec2{ 512,512 });
+
+	part.sColor.ui_color = WHITE;
+	part.sColor.ui_color = WHITE;
+	part.lifespan = 2.f;
+	part.lifetime = 0;
+
 	while (sfw::stepContext())
 	{
-		sfw::drawTexture(sprite, 0, 0, 100, 100);
-	}
+		float dt = sfw::getDeltaTime();
 
+		if (!part.refresh(dt))
+		{
+			part.pos = vec2{ 400,0 };
+			part.vel = randDir(0, 360) * lerp(20, 80, rand01());
+			part.sDim = randRange(vec2{ 8,8 }, vec2{ 32,32 });
+			part.eDim = lerp(vec2{ 256,256 }, vec2{ 512,512 }, rand01());
+			part.lifetime = 0;
+		}
+
+		
+		
+	}
 	sfw::termContext();
 }
--- Factories/math.h
+++ Factories/math.h
@@ -1,7 +1,19 @@
 #pragma once
+#include <random>
+#include <cmath>
+inline float rand01()
+{
+	return (float)rand() / RAND_MAX;
+}
 
+inline float lerp(float s, float e, float t)
+{
+	return s*(t - 1) + e*t;
+}
 struct vec2 { float x, y; };
 
+#define DEG2RAD 0.0174533
+///////////////////////////////////////////////////////
 inline vec2 operator+(const vec2 &A, const vec2 &B)
 {
 	return vec2{ A.x + B.x, A.y + B.y };
@@ -17,20 +29,33 @@ inline vec2 lerp(const vec2 &S, const vec2 &E, float t)
 	return S*(t - 1) + E *t; 
 }
 
+inline vec2 randRange(const vec2 &A, const vec2 &B)
+{
+	return vec2{ lerp(A.x,B.x,rand01()), lerp(A.y,B.y,rand01()) };
+}
+
+inline vec2 randDir(float angle_degrees, float angle_degrees2)
+{
+	float deg = lerp(angle_degrees, angle_degrees2, rand01());
+	float rad = DEG2RAD * deg; 
+
+	return vec2{ cos(rad), sin(rad) }; 
+}
+
 //////////////////////////////////
 
 union color
 {
 	unsigned ui_color; 
-	struct { char r, g, b, a; };
+	struct { unsigned char r, g, b, a; };
 };
 
 inline color lerp(const color &S, const color &E, float t)
 {
 	color ret; 
-	ret.r = S.r*(t - 1) + E.r *t; 
-	ret.g = S.r*(t - 1) + E.r *t;
-	ret.b = S.r*(t - 1) + E.r *t;
-	ret.a = S.r*(t - 1) + E.r *t;
+	ret.r = S.r*(1 - t) + E.r *t; 
+	ret.g = S.g*(1 - t) + E.g *t;
+	ret.b = S.b*(1 - t) + E.b *t;
+	ret.a = S.a*(1 - t) + E.a *t;
 	return ret; 
 }
